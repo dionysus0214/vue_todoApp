7e5683cf6a181d4c91d08afc253fe1925bf682f0
dionysus0214 <hiba0214@naver.com>
2021-08-13 17:32:46 +0900

    form component 제작

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,17 @@
 <template>
   <div class="container">
     <h2>To-Do List</h2>
-    <form @submit.prevent="onSubmit">
-      <div class="d-flex">
-        <div class="flex-grow-1 mr-2">
-          <input
-            class="form-control"
-            type="text" 
-            v-model="todo"
-            placeholder="Type new to-do"
-          >
-        </div>
-        <div>
-          <button 
-            class="btn btn-primary"
-            type="submit"
-          >
-            Add
-          </button>
-        </div>
-      </div>
-      <div v-show="hasError" style="color: red">
-        This field cannot be empty
-      </div>
-    </form>
+    <todo-simple-form />
+    <div v-if="!todos.length">
+      추가된 To-Do가 없습니다.
+    </div>
     <div 
-      v-for="todo in todos"
+      v-for="(todo, index) in todos"
       :key="todo.id"
       class="card mt-2"
     >
-      <div class="card-body p-2">
-        <div class="form-check">
+      <div class="card-body p-2 d-flex align-items-center">
+        <div class="form-check flex-grow-1">
           <input
             class="form-check-input"
             type="checkbox"
@@ -43,6 +24,14 @@
             {{ todo.subject }}
           </label>
         </div>
+        <div>
+          <button
+            class="btn btn-danger btn-sm"
+            @click="deleteTodo(index)"
+          >
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -50,7 +39,12 @@
 
 <script>
 import { ref } from 'vue';
+import TodoSimpleForm from './components/TodoSimpleForm.vue';
+
 export default {
+  components: {
+    TodoSimpleForm
+  },
   setup() {
     const todo = ref('');
     const todos = ref([]);
@@ -74,12 +68,18 @@ export default {
         todo.value = '';
       }
     };
+
+    const deleteTodo = (index) => {
+      todos.value.splice(index, 1);
+    };
+
     return {
       todo,
       todos,
       onSubmit,
       hasError,
-      todoStyle
+      todoStyle,
+      deleteTodo
     };
   }
 }
@@ -89,5 +89,8 @@ export default {
   .todo {
     color: gray;
     text-decoration: line-through;
+  }
+  .flex-grow-1 {
+    margin-right: 8px;
   }
 </style>
