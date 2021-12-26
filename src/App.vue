2129f81fe088b470c975fc0b8f3fac6141f21030
dionysus0214 <hiba0214@naver.com>
2021-12-26 19:55:33 +0900

    fragment, ref/reactive 관련 주석 추가

--- src/App.vue
+++ src/App.vue
@@ -17,3 +17,10 @@ export default {
   }
 }
 </script>
+
+// Vue.js 2에서는 root element로 감싸줘야 했으나
+// Vue.js 3에서는 최상위에 여러 개의 태그를 나란히 둬도 됨
+
+// ref는 기본 자료형, Object, Array 모두 사용 가능하며 사용 시 .value를 붙여줘야 함
+// Object, Array를 사용할 때엔 ref 대신 reactive를 사용해도 됨
+// 이 경우 .value를 붙이지 않아도 바로 사용 가능
